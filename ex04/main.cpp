7825d18f45a049bf7e9019d61645bf87f3d7acf3
agautier <pillowin@gmx.com>
2021-11-03 23:06:40 +0100

    feat(ex04): replace ok

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,21 +6,34 @@
 /*   By: agautier <agautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 15:37:51 by agautier          #+#    #+#             */
-/*   Updated: 2021/11/02 20:19:31 by agautier         ###   ########.fr       */
+/*   Updated: 2021/11/03 22:59:07 by agautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "replace.h"
+#include <sstream>
 
 int	main(int argc, char **argv)
 {
-	std::ifstream	infile;
-	std::ofstream	outfile;
+	std::ifstream		infile;
+	std::ofstream		outfile;
+	std::stringstream	ss;
+	std::string			str;
 
 	if (argc != 4)
 	{
 		std::cerr
 			<< "Wrong number of argument."
+			<< std::endl
+			<< "Use : ./replace filename s1 s2"
+			<< std::endl;
+		return (EXIT_FAILURE);
+	}
+
+	if (!*argv[1] || !*argv[2] || !*argv[3])
+	{
+		std::cerr
+			<< "Empty strings are no allowed."
 			<< std::endl
 			<< "Use : ./replace filename s1 s2"
 			<< std::endl;
@@ -37,7 +50,38 @@ int	main(int argc, char **argv)
 			<< "\": "
 			<< e.what()
 			<< std::endl;
+		return (EXIT_FAILURE);
 	}
 
+	try {
+		outfile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+		outfile.open(std::string (argv[1]) + ".replace");
+	} catch (std::ios_base::failure e) {
+		std::cerr
+			<< "Error while openning file \""
+			<< argv[1]
+			<< "\": "
+			<< e.what()
+			<< std::endl;
+		infile.close();
+		return (EXIT_FAILURE);
+	}
+
+	ss << infile.rdbuf();
+	str = ss.str();
+	while (!str.empty())
+	{
+		if (!str.compare(0, strlen(argv[2]), argv[2]))
+		{
+			outfile << argv[3];
+			str.erase(0, strlen(argv[2]));
+			continue ;
+		}
+		outfile << str.at(0);
+		str.erase(0, 1);
+	}
+
+	infile.close();
+	outfile.close();
 	return (EXIT_SUCCESS);
 }
