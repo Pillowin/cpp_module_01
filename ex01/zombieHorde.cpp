1be9f0ef4f1a7c2519a934a4c6f234b4bf9b1583
agautier <pillowin@gmx.com>
2021-11-11 13:51:49 +0100

    fix(zombieHorde): no more infinite loop when asking more than 255 zombies

--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -6,25 +6,17 @@
 /*   By: agautier <agautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 15:54:00 by agautier          #+#    #+#             */
-/*   Updated: 2021/11/07 15:13:34 by agautier         ###   ########.fr       */
+/*   Updated: 2021/11/11 11:58:29 by agautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 #include <sstream>
-#include <stdint.h>
 
-Zombie	*zombieHorde(int n, std::string name)
-{
-	Zombie *zs = new Zombie[n];
-	std::stringstream	ss;
+Zombie* zombieHorde(int n, std::string name) {
+	Zombie* zs = new Zombie[n];
 
-	for (uint8_t i = 0; i < n; i++)
-	{
-		ss << name;
-		ss << i;
-		ss >> name;
+	for (int i = 0; i < n; i++)
 		zs[i].set_name(name);
-	}
 	return (zs);
 }
